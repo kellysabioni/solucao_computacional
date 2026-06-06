6d103e4e076f59fb2d94e4b26e04ca77186f516a
Jotamunds <joaogabrielvs2022@gmail.com>
2026-06-05 21:59:10 -0300

    Atualização do Validador

--- src/main/java/com/solucao_computacional/util/Validador.java
+++ src/main/java/com/solucao_computacional/util/Validador.java
@@ -1,4 +1,12 @@
 package com.solucao_computacional.util;
 
 public class Validador {
+
+    public static boolean textoVazio(String texto) {
+        return texto == null || texto.trim().isEmpty();
+    }
+
+    public static boolean emailValido(String email) {
+        return email != null && email.contains("@");
+    }
 }
